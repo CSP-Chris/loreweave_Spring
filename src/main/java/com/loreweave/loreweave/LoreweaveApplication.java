fdec2d2b003dedc77cdbae0794ea4751a02a3656
Chris Ennis <ennisc@csp.edu>
2025-09-08 13:58:20 -0400

    Initial commit

---
+++ src/main/java/com/loreweave/loreweave/LoreweaveApplication.java
@@ -0,0 +1,17 @@
+package com.loreweave.loreweave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoreweaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoreweaveApplication.class, args);
+
+        System.out.println("Welcome");
+        System.out.println("To");
+        System.out.println("Loreweave");
+    }
+}
+
---
+++ src/test/java/com/loreweave/loreweave/LoreweaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loreweave.loreweave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoreweaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
