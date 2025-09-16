3ea64b61862007ddc1f525e306ddb6d6b2914afd
Chris Ennis <ennisc@csp.edu>
2025-09-15 23:31:55 -0400

    Had to comment out the PasswordEncoder so the main site access would work.

--- REFERENCES.md
+++ REFERENCES.md
@@ -2,3 +2,5 @@
 This is a references page.
 
 * **
+1. https://www.baeldung.com/spring-boot-h2-console-error
+* **
--- src/main/java/com/loreweave/loreweave/config/SecurityConfig.java
+++ src/main/java/com/loreweave/loreweave/config/SecurityConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,10 +15,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    //@Bean
+    //public PasswordEncoder getPasswordEncoder() {
+        //return new BCryptPasswordEncoder();
+    //}
 
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
