1703aa2bf4f40c85ef5d62526b60cb173c275c89
Wyatt <bechtlew@csp.edu>
2025-09-15 22:27:31 -0500

    Implemented the Frontend Structure of the Application
    What I have done:
    1) Added two dependancies to allow for the use of Thymeleaf
    2) Created the ContentController to handle HTTP requests
    3) Mapped login, register, and welcome pages
    4) Moved mapping from HomePageController to ContentController
    5) Updated main method in LoreWeaveApplication
    6) Created associatted directories for Thymeleaf
    7) Created a login page
    8) Created a register page
    9) Connected login and register page via links

---
+++ src/main/java/com/loreweave/loreweave/ContentController.java
@@ -0,0 +1,33 @@
+// File Name: login.html
+// Created By: Wyatt Bechtle
+// Created On: 9/15/2025
+// Purpose: To provide login capabilities to the loreweave application
+
+
+// Updated By: <name> on <date>
+package com.loreweave.loreweave;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class ContentController {
+    
+    @GetMapping("/loginBSF")
+    public String login() {
+        return "loginBSF";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
+    
+}
