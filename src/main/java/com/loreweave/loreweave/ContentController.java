1520135f74d0f64337ed8a90fdbc4d820352c33d
Chris Ennis <ennisc@csp.edu>
2025-09-16 13:20:16 -0400

    Unused imports commented out and fixed main to ignore args.

--- src/main/java/com/loreweave/loreweave/ContentController.java
+++ src/main/java/com/loreweave/loreweave/ContentController.java
@@ -7,9 +7,9 @@
 // Updated By: <name> on <date>
 package com.loreweave.loreweave;
 
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
