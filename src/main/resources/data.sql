cd68633e3ae542399d4556490d64f790d06144ff
Chris Ennis <ennisc@csp.edu>
2025-09-15 20:43:00 -0400

    Updated user table and class pages to be Player as there were conflicts.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,9 @@
-INSERT INTO "user" (id, first_name, last_name, email, password) VALUES
-                                                                  (1, 'Chris', 'Test', 'chris@example.com', 'password'),
-                                                                  (2, 'Jamie', 'Test', 'jamie@example.com', 'password'),
-                                                                  (3, 'Wyatt', 'Test', 'wyatt@example.com', 'password');
-INSERT INTO character (id, name, description, lore_points, user_id) VALUES
-                                                                        (101, 'Frodo Baggins', 'A Hobbit of the Shire', 1, 1),
-                                                                        (102, 'Gandalf', 'A wise wizard of the Istari', 1, 2),
-                                                                        (103, 'Aragorn', 'Heir of Isildur', 1, 3);
+INSERT INTO player (id, first_name, last_name, email, password) VALUES
+(1, 'Chris', 'Test', 'chris@example.com', 'password'),
+(2, 'Jamie', 'Test', 'jamie@example.com', 'password'),
+(3, 'Wyatt', 'Test', 'wyatt@example.com', 'password');
+
+INSERT INTO character (id, name, description, lore_points, player_id) VALUES
+(101, 'Frodo Baggins', 'A Hobbit of the Shire', 1, 1),
+(102, 'Gandalf', 'A wise wizard of the Istari', 1, 2),
+(103, 'Aragorn', 'Heir of Isildur', 1, 3);
